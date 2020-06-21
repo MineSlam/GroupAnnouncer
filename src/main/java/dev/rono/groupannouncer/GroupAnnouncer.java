89c46567c3f160833886fc7a3a195b2736dd54c3
Rono <rowanpsmith@yahoo.com.au>
2020-06-21 12:19:13 +1000

    Initial commit

---
+++ src/main/java/dev/rono/groupannouncer/GroupAnnouncer.java
@@ -0,0 +1,10 @@
+package dev.rono.groupannouncer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GroupAnnouncer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+}
