7e6421407e5fc0c50e4d20bcdcc9f247109e6424
Rowan <rowanpsmith@yahoo.com.au>
2020-09-12 05:03:53 +1000

    Remove redundant imports and update api-version to 1.16

--- src/main/java/dev/rono/groupannouncer/JoinListener.java
+++ src/main/java/dev/rono/groupannouncer/JoinListener.java
@@ -1,10 +1,8 @@
 package dev.rono.groupannouncer;
 
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.MemorySection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +12,7 @@ import java.util.List;
 
 public class JoinListener implements Listener {
 
-    private ConfigurationSection config;
+    private final ConfigurationSection config;
 
     JoinListener() {
         config = GroupAnnouncer.getConfiguration().getConfigurationSection("groups");
@@ -27,13 +25,11 @@ public class JoinListener implements Listener {
         String group = GroupAnnouncer.getPermission().getPrimaryGroup(player);
 
         List<String> messages = config.getStringList(group + ".message");
-        if (messages != null) {
-            for (String message : messages) {
-                message = message.replace("{player}", player.getName());
-                message = ChatColor.translateAlternateColorCodes('&', message);
-                TextComponent textMessage = new TextComponent(TextComponent.fromLegacyText(message));
-                event.getPlayer().getServer().broadcast(textMessage);
-            }
+        for (String message : messages) {
+            message = message.replace("{player}", player.getName());
+            message = ChatColor.translateAlternateColorCodes('&', message);
+            TextComponent textMessage = new TextComponent(TextComponent.fromLegacyText(message));
+            event.getPlayer().getServer().broadcast(textMessage);
         }
     }
 }
